ceb32a8ff7ed62f208f05c6094bc8ebc1c632e18
MartinBruun <mbm93fen@gmail.com>
2022-05-16 12:40:42 +0200

    Test automatic pytest testing framework

--- src/tests/test_controller.py
+++ src/tests/test_controller.py
@@ -2,6 +2,9 @@
 
 from controller import controller
 
+def test_true():
+    assert True
+
 def test_controller_can_be_setup_for_integration_tests(mocker):
     mockVideoLoader = mocker.patch("src.controller.video_loader", return_value=True)
     mockUserInput = mocker.patch("src.controller.user_input", return_value=True)
